5587525bde69879072fa3e6ee76e07effac900f7
github username <github email address>
2019-03-27 13:51:32 +0000

    Added comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_25_133743) do
+ActiveRecord::Schema.define(version: 2019_03_27_134909) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "poll_id"
+    t.integer "user_id"
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "followers", force: :cascade do |t|
     t.integer "followed_id"
