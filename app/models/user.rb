e98758998cd240296c5de9ea445466e791e88cfb
github username <github email address>
2019-03-28 11:11:11 +0000

    Search by username

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :email, presence: true, uniqueness: true
+
 
   def followers
     Follower.where("followed_id = #{id}")
@@ -14,5 +17,16 @@ class User < ApplicationRecord
 
   def following
     Follower.where("follower_id = #{id}")
+  end
+
+  def self.search(search)
+    if search
+      found = User.where(username: search)
+      if found.empty?
+        User.all
+      end
+    else
+      User.all
+    end
   end
 end
