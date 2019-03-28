15f5762861de52c8cafa7c6bc86deb1beec21365
github username <github email address>
2019-03-28 17:32:37 +0000

    Styling fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :username, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :password_digest, presence: true
   validates :email, presence: true, uniqueness: true
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+ProfilePicture.create(img_url: "https://static-cdn.jtvnw.net/jtv_user_pictures/f2507460-9669-4714-b1bb-dd2c75bd005a-profile_image-300x300.jpg")
+
+ProfilePicture.create(img_url: "https://cdn2.digitalartsonline.co.uk/cmsdata/slideshow/3306055/shme7_Final.jpg")
+
+ProfilePicture.create(img_url: "http://farm4.static.flickr.com/3648/3548255118_d026c73d1e.jpg")
+
+ProfilePicture.create(img_url: "https://cdn.dribbble.com/users/1675845/screenshots/4103971/________-1.gif")
+
+ProfilePicture.create(img_url: "https://media2.giphy.com/media/dcMBpNILailsQ/source.gif")
