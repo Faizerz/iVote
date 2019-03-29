188fa3d00ea85156bc4f0c726573d004f29917fc
KaiKaspar <kai.k.b.shovlin@gmail.com>
2019-03-29 12:01:35 +0000

    layout stuff

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -3,12 +3,14 @@ class FollowersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+
+
   def create
     if params[:todo] == 'follow'
       Follower.create(followed_id: params[:button].to_i, follower_id: current_user.id)
     elsif params[:todo] == 'unfollow'
       Follower.find_by(followed_id: params[:button].to_i, follower_id: current_user.id).destroy()
     end
-    redirect_to users_path
+      redirect_back(fallback_location: root_path)
   end
 end
